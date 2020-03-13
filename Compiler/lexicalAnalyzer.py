54eb47f0cc940411a6136335d6547776cac95d16
Lumanter <luismariano10del4@gmail.com>
2020-03-13 00:30:41 -0600

    adding regular expressions for tokens

--- Compiler/lexicalAnalyzer.py
+++ Compiler/lexicalAnalyzer.py
@@ -9,9 +9,10 @@ keywords = {
 	'main': 'MAIN',
     'procedure': 'PROCEDURE',
     'call': 'CALL',
-    'if': 'IF',
-	'else': 'ELSE',
-	'for': 'FOR'
+	#'if': 'IF',
+	#'else': 'ELSE',
+	#'for': 'FOR'
+	'delay': 'DELAY'
 }
 
 tokens = [
@@ -27,11 +28,13 @@ tokens = [
 	'MINUS',
 	'MULTIPLY',
 	'DIVIDE',
+	'POWER',
+	'MODULO',
 
 	# Logical Comparators
-	'EQUALS',
-	'LESSTHAN',
-	'GREATERTHAN',
+	#'EQUALS',
+	#'LESSTHAN',
+	#'GREATERTHAN',
 
 	# Enclosing Characters
 	'LPARENTHESES', 
@@ -47,7 +50,42 @@ tokens = [
 	'DOT'
 	]
 
+# Adding the keywords to the total tokens
 tokens = tokens + list(keywords.values())
 
+# Regular expressions for simple tokens
+t_ignore = '\t ' #ignore spaces and tabs
+t_ASSIGN = r'\='
+t_PLUS = r'\+'
+t_MINUS = r'\-'
+t_MULTIPLY = r'\*'
+t_DIVIDE = r'/'
+t_POWER = r'\*\*'
+t_MODULO = r'\%'
+t_LPARENTHESES = r'\(' 
+t_RPARENTHESES = r'\)' 
+t_LSQUAREBRACKET = r'\[' 
+t_RSQUAREBRACKET = r'\]' 
+t_LBRACE = r'\{' 
+t_RBRACE = r'\}' 
+t_COMMA = r','
+t_SEMMICOLON = r';'
+t_DOT = r'\.'
 
+# Regular expressions for not so simple tokens
+def t_ID(t):
+	r'[a-zA-Z_][a-zA-Z0-9_]*'
+	if t.value.upper() in keywords:
+		t.value = t.value.upper()
+		t.type = t.value
+	return t
 
+def t_INTEGER(t):
+	r'\d+'
+	t.value = int(t.value)
+	return t
+
+def t_BOOLEAN(t):
+	r'true|false'
+	t.value = (t.value == 'true')
+	return t
