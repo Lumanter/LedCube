43decd18931b47f40a8cbf09516f1bf0c0226859
Lumanter <luismariano10del4@gmail.com>
2020-03-13 14:21:52 -0600

    Adding regular expressions for keywords and other first sprint tokens

--- Compiler/lexicalAnalyzer.py
+++ Compiler/lexicalAnalyzer.py
@@ -23,6 +23,9 @@ tokens = [
 	'INTEGER',
 	'BOOLEAN',
 
+	# open to discussion
+	'TIMEUNIT',
+
 	# Numerical Operators 
 	'PLUS',
 	'MINUS',
@@ -54,13 +57,13 @@ tokens = [
 tokens = tokens + list(keywords.values())
 
 # Regular expressions for simple tokens
+t_POWER = r'\*\*'
 t_ignore = '\t ' #ignore spaces and tabs
 t_ASSIGN = r'\='
 t_PLUS = r'\+'
 t_MINUS = r'\-'
 t_MULTIPLY = r'\*'
 t_DIVIDE = r'/'
-t_POWER = r'\*\*'
 t_MODULO = r'\%'
 t_LPARENTHESES = r'\(' 
 t_RPARENTHESES = r'\)' 
@@ -73,9 +76,19 @@ t_SEMMICOLON = r';'
 t_DOT = r'\.'
 
 # Regular expressions for not so simple tokens
+def t_TIMEUNIT(t):
+	r'\"(Mil|Seg|Min)\"'
+	t.value = t.value[1:-1]
+	return t
+
+def t_BOOLEAN(t):
+	r'true|false'
+	t.value = (t.value == 'true')
+	return t
+
 def t_ID(t):
-	r'[a-zA-Z_][a-zA-Z0-9_]*'
-	if t.value.upper() in keywords:
+	r'[a-zA-Z_][a-zA-Z0-9@_&]*'
+	if t.value.lower() in keywords:
 		t.value = t.value.upper()
 		t.type = t.value
 	return t
@@ -85,7 +98,38 @@ def t_INTEGER(t):
 	t.value = int(t.value)
 	return t
 
-def t_BOOLEAN(t):
-	r'true|false'
-	t.value = (t.value == 'true')
-	return t
+# Tracking the line number, for error messaging 
+def t_newline(t):
+	r'\n+'
+	t.lexer.lineno += len(t.value)
+def t_error(t):
+	print "Lexical error: Illegal character '" + t.value[0] + "' at line number " + str(t.lexer.lineno)
+	t.lexer.skip(1)
+
+lexicalAnalayzer = lex.lex()
+
+# Tester
+# data = '''
+# 	Timer = 1;
+# 	Rango_timer = \"Seg\";
+# 	Dim_Filas = 8;
+# 	Dim_Columnas = 8;
+# 	Cubo = [];
+
+# 	Procedure turnOnLed(x, y, z) {
+# 		Cubo[x][y][z] = true;	
+# 	}
+# '''
+
+# '''
+# 	Procedure Main() {
+# 		myBoolean = true;
+# 		x = 6 / 2 + 3 ** 8 - 1;
+# 		y = 10 % 2;
+# 		call turnOnLed(0, 0, 0);
+# 		Delay() 
+# 	}
+# '''
+# lexicalAnalayzer.input(data)
+# for token in lexicalAnalayzer:
+#     print(token.type, token.value)
