c2cd0e70da8f9a2d60b6f94edadcf4d9ab5d8505
jonDorito <jonDorito@users.noreply.github.com>
2020-05-07 20:18:56 -0600

    A code generator module has been created with the basic instructions baked into it

---
+++ Compiler/codeGenerator.py
@@ -0,0 +1,17 @@
+finalCode = ""
+
+def writeCode(lineOfCode):
+    global finalCode
+    finalCode += lineOfCode
+
+def delay(time, timeUnit):
+    instruction = "delay," + str(time) + ',' + str(timeUnit) + "\n"
+    writeCode(instruction)
+
+def turn(x, y, z, state):
+    instruction = "turn," + str(x) + ',' + str(y) + ',' + str(z) + ',' + str(state).lower() + "\n"
+    writeCode(instruction)
+
+def blink(x, y, z, time, timeUnit, state):
+    instruction = "blink," + str(x) + ',' + str(y) + ',' + str(z) + ',' + str(time) + ',' + str(timeUnit) + ',' + str(state).lower() + "\n"
+    writeCode(instruction)
